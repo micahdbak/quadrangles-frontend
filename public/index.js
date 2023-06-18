bd44d70beedb4df070d98dc8fb46064de94dda02
Micah Baker <micahb923@gmail.com>
2023-06-18 15:50:05 -0700

    Implemented loading and displaying of posts

--- public/index.js
+++ public/index.js
@@ -1,11 +1,68 @@
+// set to localhost for development; set to an empty string for production
+const HOST = "http://localhost:8000";
+
+function populate(arr) {
+	let posts = document.getElementById("posts");
+
+	posts.innerHTML = "";
+
+	for (const p of arr) {
+		let text = p.text;
+
+		if (text.length > 100)
+			text = text.substring(0, 100) + "...";
+
+		posts.innerHTML += `
+			<div class="post">
+				<img src="${HOST}${p.file}" />
+				<section>
+					<!--<h1>${p.title}</h1>-->
+					<p>${text}</p>
+					<a href="post.html?p=${p.pid}">
+						Open Post &nearr;
+					</a>
+				</section>
+			</div>
+		`;
+	}
+}
+
 window.onload = () => {
-	topic = document.getElementById("topic");
+	let topicInput = document.getElementById("topic");
 
-	topic.oninput = () => {
-		if (topic.value[0] != ':')
-			topic.value = ":" + topic.value;
+	topicInput.oninput = () => {
+		if (topicInput.value[0] != ':')
+			topicInput.value = ":" + topicInput.value;
 
-		if (topic.value.length > 5)
-			topic.value = topic.value.substring(0, 5);
+		if (topicInput.value.length > 5)
+			topicInput.value = topicInput.value.substring(0, 5);
 	};
+
+	let message = document.getElementById("message");
+	message.innerHTML = "All OK.";
+
+	topicInput.onchange = async () => {
+		message.innerHTML = "Loading...";
+
+		let topic = topicInput.value.substring(1, topicInput.value.length);
+		let response;
+
+		try {
+			response = await fetch(HOST + "/api/t/" + topic);
+		} catch {
+			message.innerHTML = "Could not contact server.";
+			return;
+		}
+
+		if (!response.ok) {
+			message.innerHML = `Received ${response.status}.`;
+			return;
+		}
+
+		let posts = await response.json();
+		message.innerHTML = `Loaded ${posts.length} post${posts.length == 1 ? '' : 's'}.`;
+
+		populate(posts);
+	};
+	topicInput.onchange();
 };
